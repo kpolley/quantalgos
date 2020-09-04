6370ff8d647cdd933bbc44f0ab9b2741513e651c
Kyle Polley <kyle.polley@robinhood.com>
2020-09-04 09:15:57 -0700

    using alpaca clock for market hours

--- runner.py
+++ runner.py
@@ -11,14 +11,13 @@ STOCKS = ["WM", "AAPL", "CCL", "V", "TWTR", "NVDA"]
 
 alpaca = tradeapi.REST(credentials.alpaca_key_id, credentials.alpaca_secret_key, APCA_API_BASE_URL, 'v2')
 
-MARKET_OPEN = datetime.time(9,0)
-MARKET_CLOSED = datetime.time(13,0)
-
 def awaitMarketOpen():
-    this_hour = datetime.datetime.now().time()
-    while this_hour <= MARKET_OPEN or this_hour >= MARKET_CLOSED:
-        print("After hours")
-        time.sleep(60)
+    while not alpaca.get_clock().is_open:
+        next_open = alpaca.get_clock().next_open.replace(tzinfo=datetime.timezone.utc).timestamp()
+        current_time = alpaca.get_clock().timestamp.replace(tzinfo=datetime.timezone.utc).timestamp()
+        time_until_open = int((openingTime - currTime) / 60)
+        print(f"After hours. Next open in {time_until_open} seconds")
+        time.sleep(time_until_open + 60)
 
 def main():
     runners = [MACD(stock, 10, alpaca) for stock in STOCKS]
